08dc31ea78f1c06654b99507992a067ccb383e9c
Myoung <mhbae93@gmail.com>
2022-02-11 12:12:03 +0000

    added styling and sidebar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Editor from "./components/Editor";
+import Sidebar from "./components/Sidebar";
+import Split from 'react-split';
+import {nanoid} from 'nanoid';
+import {data} from './data'
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
       <Editor />
     </div>
   );
